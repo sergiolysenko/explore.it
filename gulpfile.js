c2185eb1f75612d5a26b23ca1c243be368014595
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2021-02-03 11:35:29 +0300

    create folder hierarchy, init package.json and install dependencies

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var webp = require("gulp-webp");
+var posthtml = require("gulp-posthtml");
+var iclude = require("posthtml-include");
+var del = require("del");
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("webp", function () {
+  return gulp.src("build/img/**/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    iclude()
+  ]))
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("copyJs", function () {
+  return gulp.src([
+      "source/js/**",
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}").on('change', gulp.series("css"));
+  gulp.watch("source/*.html").on('change', gulp.series("html", "refresh"));
+  gulp.watch("source/js/*.js").on('change', gulp.series("copyJs", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+})
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/js/**",
+      "source/*.ico"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "webp",
+  "html"
+));
+
+gulp.task("start", gulp.series("build", "server"));
